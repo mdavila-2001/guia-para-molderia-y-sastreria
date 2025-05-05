af70c7489cf9e0a888898c9a8daaa892cedc037c
Marcelo Dávila <dmarcelo201@gmail.com>
2025-05-04 23:03:25 -0400

    created angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'guia-para-molderia-y-sastreria';
+  title = 'Guía para Moldería y Sastrería';
 }
