35f8140fe7305ebaea73de9d7f531f76e392893c
Marcelo Dávila <marcelo.tester.fos@gmail.com>
2025-05-08 16:52:59 -0400

    created fundamentals

--- src/app/pages/fundamentals/fundamentals.component.ts
+++ src/app/pages/fundamentals/fundamentals.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DescCardComponent } from '../../components/desc-card/desc-card.component';
+import { TableComponent } from '../../components/table/table.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-fundamentals',
-  imports: [],
+  standalone: true,
+  imports: [DescCardComponent, TableComponent],
   templateUrl: './fundamentals.component.html',
   styleUrl: './fundamentals.component.css'
 })
-export class FundamentalsComponent {
+export class FundamentalsComponent implements OnInit{
+  data: any[] = [];
 
+  basicTools = {
+    title: 'Herramientas Básicas',
+    isList: true,
+    items: [
+      'Cinta métrica',
+      'Papel para patrones',
+      'Tijeras de tela y papel',
+      'Alfileres y agujas',
+      'Tiza de sastre'
+    ],
+    accentColor: '#536C4B'
+  };
+
+  measurements = {
+    title: 'Medidas',
+    description: 'Para realizar el molde de cuerpo base, es necesario tomar las siguientes medidas:',
+    accentColor: '#536C4B'
+  }
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('assets/data/measures.json').subscribe(res => {
+      this.data = res;
+    });
+  }
 }
