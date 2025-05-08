1d474798c1479f44876e368c28da8a0395b1eb24
Marcelo Dávila <dmarcelo201@gmail.com>
2025-05-08 10:52:32 -0400

    added config and fixed tables

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -5,13 +5,11 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
 export class TableComponent implements OnChanges{
   @Input() datos: any[] = [];
-
   columnas: string[] = [];
 
   ngOnChanges(changes: SimpleChanges): void {
