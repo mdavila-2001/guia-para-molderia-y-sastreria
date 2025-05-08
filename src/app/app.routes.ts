656de6301d492e54dd34eee121cb1bdce0659198
Marcelo Dávila <dmarcelo201@gmail.com>
2025-05-08 07:44:24 -0400

    controlling the router and home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,18 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
+    title: 'Inicio'
+  },
+  {
     path: 'test',
     loadComponent: () => import('./pages/test/test.component').then(m => m.TestComponent),
     title: 'Página de Prueba'
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
   }
 ];
