b86c36c69bb0388905577e3b44610f7f5d3ed7c5
Marcelo Dávila <marcelo.tester.fos@gmail.com>
2025-05-09 11:25:41 -0400

    added resources

--- src/app/pages/resources/resources.component.ts
+++ src/app/pages/resources/resources.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { DescCardComponent } from '../../components/desc-card/desc-card.component';
 
 @Component({
   selector: 'app-resources',
-  imports: [],
+  imports: [DescCardComponent],
   templateUrl: './resources.component.html',
   styleUrl: './resources.component.css'
 })
 export class ResourcesComponent {
-
+  basics = {
+    title: 'Ejemplo con Lista',
+    isList: true,
+    items: [
+      'Cinta métrica profesional',
+      'Reglas de patronaje',
+      'Tijeras de tela y papel',
+      'Alfileres y agujas'
+    ],
+    accentColor: '#2E5090'
+  };
 }
